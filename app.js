3de27ed8f7b868f076335c8d75ba5a2c27c8f9f7
Yasir Ali <yasir9.1@live.com>
2025-06-18 21:11:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const auth = require("./middleware/auth.middleware"); // ✅ Add this
 
 const authRoutes = require("./routes/auth.routes");
 const boardRoutes = require("./routes/board.routes");
-
+const taskRoutes = require("./routes/task.routes");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -15,5 +15,5 @@ app.use("/api/auth", authRoutes);
 
 // Later use auth like:
 app.use("/api/boards", auth, boardRoutes);
-
+app.use("/api/tasks", authMiddleware, taskRoutes); // ✅ JWT protected
 module.exports = app;
